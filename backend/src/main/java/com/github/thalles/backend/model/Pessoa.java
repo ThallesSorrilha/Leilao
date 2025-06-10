2360075f6b6d58a051ec5328a3bb282db0c64262
ThallesSorrilha <thallessorrilha@gmail.com>
2025-06-10 00:52:54 -0300

    feat(backend): criando backend

---
+++ backend/src/main/java/com/github/thalles/backend/model/Pessoa.java
@@ -0,0 +1,21 @@
+package com.github.thalles.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String senha;
+}
