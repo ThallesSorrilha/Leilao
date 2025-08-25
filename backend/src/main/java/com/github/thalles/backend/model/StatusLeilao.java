d5387e7f7467e23be1ed503d93d562b6650efb37
ThallesSorrilha <thallessorrilha@gmail.com>
2025-08-25 15:45:53 -0300

    feature(model): adicionando enums de StatusLeilao e Tipo Perfil

---
+++ backend/src/main/java/com/github/thalles/backend/model/StatusLeilao.java
@@ -0,0 +1,5 @@
+package com.github.thalles.backend.model;
+
+public enum StatusLeilao {
+    ABERTO, ENCERRADO, CANCELADO, EM_ANALISE;
+}
---
+++ backend/src/main/java/com/github/thalles/backend/model/TipoPerfil.java
@@ -0,0 +1,5 @@
+package com.github.thalles.backend.model;
+
+public enum TipoPerfil {
+    ADMIN, COMPRADOR, VENDEDOR;
+}
